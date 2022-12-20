46b06e0babd862c96902e57b4b80e7dc19619238
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-19 22:52:53 -0700

    server listening, packages and whatnot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 
-const app = express();
-
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (_, res) => {
-	res.send('Hello world');
-});
+const start = async () => {
+	const app = express();
+	app.get('/', (_, res) => {
+		res.send('Hello world');
+	});
 
-app.listen(PORT, () => `App is listening on port: ${PORT}`);
+	app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
+};
+
+start();
